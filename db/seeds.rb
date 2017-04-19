afe35e335f97d3b36fb8801e3b4a7c3286fa549d
rswong <ziud7rs@gmail.com>
2017-04-19 18:50:10 +0800

    created the new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,37 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts 'Hello World!'
-puts '這個種子檔會自動建立一個帳號, 並且随机創建 10 個jobs，10个隐藏的jobs'
+puts '這個種子檔會自動建立一個帳號, 並且随机創建 16 個jobs，16个隐藏的jobs'
 
-create_account = User.create([email: 'admin@admin.admin', password: '111111', password_confirmation: '111111', is_admin: 'true'])
+create_account = User.create([email: 'admin@admin.com', password: '111111', password_confirmation: '111111', is_admin: 'true'])
 puts 'Admin account is created successfully!'
 
 job_info = [
-  '招聘RoR工程师',
-  '招聘文案设计',
-  '招聘UI设计师',
-  '招聘Android开发工程师',
-  '招聘产品经理',
-  '招聘前端开发工程师',
-  '招聘市场营销',
-  '招聘php后台研发工程师',
-  '招聘python工程师',
-  '招聘高级JAVA研发工程师',
-  '招聘高级数据挖掘工程师',
-  '招聘高级客服经理'
+  'Furniture Design',
+  'Transportation Design',
+  'Product Design',
+  'Software design',
+  'Stationery Design',
+  'Toy Design',
+  'Advertisement Design',
+  'Package Design',
+  'Illustration Design',
+  'Animation Design',
+  'Web Design',
+  'Cosmetics Design',
+  'Fashion Design',
+  'Jewelry Design',
+  'Architects',
+  'Interior designer',
+
+
 ]
-create_jobs = for i in 1..10 do
+create_jobs = for i in 1..16 do
                 Job.create!([title: job_info[rand(job_info.length)], description: "这是一个公开的工作", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'false'])
               end
-for i in 1..10 do
+for i in 1..16 do
   Job.create!([title: job_info[rand(job_info.length)], description: "这是一个隐藏的工作", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'true'])
 end
 
-puts '10 Public jobs created.'
-puts '10 Hidden jobs created.'
+puts '16 Public jobs created.'
+puts '16 Hidden jobs created.'
